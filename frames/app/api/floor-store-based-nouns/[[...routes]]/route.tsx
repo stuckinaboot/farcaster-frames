f01b0b65a4326b6b4bc99103e8e1971aaad54241
stuckinaboot <paluter@gmail.com>
2024-04-05 15:53:23 -0400

    update

---
+++ frames/app/api/floor-store-based-nouns/[[...routes]]/route.tsx
@@ -0,0 +1,23 @@
+/** @jsxImportSource frog/jsx */
+
+import { devtools } from "frog/dev";
+import { handle } from "frog/next";
+import { serveStatic } from "frog/serve-static";
+import {
+  ChainId,
+  generateFloorStoreApp,
+} from "../../floorStoreFrameGenerator.tsx";
+
+const app = generateFloorStoreApp({
+  collectionName: "Based Nouns",
+  slug: "based-nouns",
+  chainId: ChainId.BASE,
+});
+
+devtools(app, { serveStatic });
+
+export const GET = handle(app);
+export const POST = handle(app);
+
+// Uncomment to use Edge Runtime
+// export const runtime = 'edge'
--- frames/app/api/floorStoreFrameGenerator.tsx
+++ frames/app/api/floorStoreFrameGenerator.tsx
@@ -134,6 +134,7 @@ export function generateFloorStoreApp(params: {
     const title = `Floor Store: ${params.collectionName}`;
 
     return c.res({
+      headers: { "Cache-Control": "max-age=15" },
       image: (
         <div
           style={{
