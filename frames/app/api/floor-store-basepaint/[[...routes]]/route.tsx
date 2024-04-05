a059ae888568e34be6e5aec0ce2dd131161dd166
stuckinaboot <paluter@gmail.com>
2024-04-04 20:31:59 -0400

    update

--- frames/app/api/floor-store-basepaint/[[...routes]]/route.tsx
+++ frames/app/api/floor-store-basepaint/[[...routes]]/route.tsx
@@ -7,175 +7,187 @@ import { serveStatic } from "frog/serve-static";
 import { formatEther } from "viem";
 import api from "api";
 import { abi } from "../../seaportAbi.ts";
+import {
+  ChainId,
+  generateFloorStoreApp,
+} from "../../floorStoreFrameGenerator.tsx";
 
-const sdk = api("@opensea/v2.0#27kiuuluk3ys90");
-sdk.server("https://api.opensea.io");
+// const sdk = api("@opensea/v2.0#27kiuuluk3ys90");
+// sdk.server("https://api.opensea.io");
 
-sdk.auth(process.env.OS_API_KEY);
+// sdk.auth(process.env.OS_API_KEY);
 
-const SEAPORT_PROTOCOL_ADDRESS = "0x0000000000000068f116a894984e2db1123eb395";
+// const SEAPORT_PROTOCOL_ADDRESS = "0x0000000000000068f116a894984e2db1123eb395";
 
-const IS_TESTNETS = false;
-// IDs https://docs.simplehash.com/reference/supported-chains-testnets
-// Base: eip155:8453, Base Sepolia: "eip155:84532"
-const CHAIN_NAME = "base";
-const CHAIN = IS_TESTNETS ? "eip155:84532" : "eip155:8453";
+// const IS_TESTNETS = false;
+// // IDs https://docs.simplehash.com/reference/supported-chains-testnets
+// // Base: eip155:8453, Base Sepolia: "eip155:84532"
+// const CHAIN_NAME = "base";
+// const CHAIN = IS_TESTNETS ? "eip155:84532" : "eip155:8453";
 
-// UI
-const SLUG = "basepaint";
-const COLLECTION_NAME = "BasePaint";
-const TITLE = `Floor Store: ${COLLECTION_NAME}`;
+// // UI
+// const SLUG = "basepaint";
+// const COLLECTION_NAME = "BasePaint";
+// const TITLE = `Floor Store: ${COLLECTION_NAME}`;
 
-async function getFloorListing(slug: string) {
-  const bestListings = await sdk.get_best_listings_on_collection_v2({
-    collection_slug: slug,
-  });
+// async function getFloorListing(slug: string) {
+//   const bestListings = await sdk.get_best_listings_on_collection_v2({
+//     collection_slug: slug,
+//   });
 
-  // Get first listing with start amount and end amount art 1
-  const bestListing = bestListings?.data?.listings.find((listing: any) => {
-    const startAmt = +listing.protocol_data.parameters.offer[0].startAmount;
-    const endAmt = +listing.protocol_data.parameters.offer[0].endAmount;
-    return startAmt === 1 && endAmt === 1;
-  });
+//   // Get first listing with start amount and end amount art 1
+//   const bestListing = bestListings?.data?.listings.find((listing: any) => {
+//     const startAmt = +listing.protocol_data.parameters.offer[0].startAmount;
+//     const endAmt = +listing.protocol_data.parameters.offer[0].endAmount;
+//     return startAmt === 1 && endAmt === 1;
+//   });
 
-  // const bestListing = bestListings?.data?.listings[0];
-  return bestListing;
-}
+//   // const bestListing = bestListings?.data?.listings[0];
+//   return bestListing;
+// }
 
-async function getNft(params: {
-  chain: string;
-  address: string;
-  identifier: string;
-}) {
-  const response = await sdk.get_nft(params);
-  return response?.data?.nft;
-}
+// async function getNft(params: {
+//   chain: string;
+//   address: string;
+//   identifier: string;
+// }) {
+//   const response = await sdk.get_nft(params);
+//   return response?.data?.nft;
+// }
 
-const app = new Frog({
-  assetsPath: "/",
-  basePath: "/api/floor-store-basepaint",
-  // Supply a Hub to enable frame verification.
-  // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
-});
+// const app = new Frog({
+//   assetsPath: "/",
+//   basePath: "/api/floor-store-basepaint",
+//   // Supply a Hub to enable frame verification.
+//   // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
+// });
 
-app.transaction("/buy", async (c) => {
-  try {
-    const floorListing = await getFloorListing(SLUG);
+// app.transaction("/buy", async (c) => {
+//   try {
+//     const floorListing = await getFloorListing(SLUG);
 
-    const listing = {
-      hash: floorListing.order_hash,
-      chain: CHAIN_NAME,
-      protocol: SEAPORT_PROTOCOL_ADDRESS,
-    };
-    const fulfiller = { address: c.address };
+//     const listing = {
+//       hash: floorListing.order_hash,
+//       chain: CHAIN_NAME,
+//       protocol: SEAPORT_PROTOCOL_ADDRESS,
+//     };
+//     const fulfiller = { address: c.address };
 
-    const data = await sdk.generate_listing_fulfillment_data_v2({
-      listing,
-      fulfiller,
-    });
-    const fulfillmentData = data.data.fulfillment_data;
-    const quantity =
-      +floorListing.protocol_data.parameters.offer[0].startAmount;
-    const value = floorListing?.price?.current?.value / quantity;
+//     const data = await sdk.generate_listing_fulfillment_data_v2({
+//       listing,
+//       fulfiller,
+//     });
+//     const fulfillmentData = data.data.fulfillment_data;
+//     const quantity =
+//       +floorListing.protocol_data.parameters.offer[0].startAmount;
+//     const value = floorListing?.price?.current?.value / quantity;
 
-    const functionName = fulfillmentData.transaction.function.substring(
-      0,
-      fulfillmentData.transaction.function.indexOf("(")
-    );
+//     const functionName = fulfillmentData.transaction.function.substring(
+//       0,
+//       fulfillmentData.transaction.function.indexOf("(")
+//     );
 
-    return c.contract({
-      abi: abi,
-      functionName: functionName,
-      args: Object.values(fulfillmentData.transaction.input_data) as any,
-      chainId: CHAIN,
-      to: SEAPORT_PROTOCOL_ADDRESS,
-      value: BigInt(value) as any,
-    });
-  } catch (e) {
-    throw new Error("Failed to purchase");
-  }
-});
+//     return c.contract({
+//       abi: abi,
+//       functionName: functionName,
+//       args: Object.values(fulfillmentData.transaction.input_data) as any,
+//       chainId: CHAIN,
+//       to: SEAPORT_PROTOCOL_ADDRESS,
+//       value: BigInt(value) as any,
+//     });
+//   } catch (e) {
+//     throw new Error("Failed to purchase");
+//   }
+// });
 
-app.frame("/", async (c) => {
-  const { status } = c;
+// app.frame("/", async (c) => {
+//   const { status } = c;
 
-  const floorListing = await getFloorListing(SLUG);
-  const quantity = +floorListing.protocol_data.parameters.offer[0].startAmount;
-  const price = floorListing?.price?.current?.value / quantity;
-  const currency = floorListing?.price?.current?.currency;
-  const parsedPrice = formatEther(BigInt(price));
+//   const floorListing = await getFloorListing(SLUG);
+//   const quantity = +floorListing.protocol_data.parameters.offer[0].startAmount;
+//   const price = floorListing?.price?.current?.value / quantity;
+//   const currency = floorListing?.price?.current?.currency;
+//   const parsedPrice = formatEther(BigInt(price));
 
-  const firstOfferItem = floorListing.protocol_data.parameters.offer[0];
-  const { token, identifierOrCriteria } = firstOfferItem;
+//   const firstOfferItem = floorListing.protocol_data.parameters.offer[0];
+//   const { token, identifierOrCriteria } = firstOfferItem;
 
-  const nft = await getNft({
-    chain: CHAIN_NAME,
-    address: token,
-    identifier: identifierOrCriteria,
-  });
+//   const nft = await getNft({
+//     chain: CHAIN_NAME,
+//     address: token,
+//     identifier: identifierOrCriteria,
+//   });
 
-  const description = `Purchase via Frame\nPrice: ${parsedPrice} ${currency}`;
-  const imgSrc = nft?.image_url;
+//   const description = `Purchase via Frame\nPrice: ${parsedPrice} ${currency}`;
+//   const imgSrc = nft?.image_url;
 
-  // NOTE: svg image urls don't seem to work properly
+//   // NOTE: svg image urls don't seem to work properly
 
-  return c.res({
-    image: (
-      <div
-        style={{
-          alignItems: "center",
-          background:
-            status === "response"
-              ? "linear-gradient(to right, #432889, #17101F)"
-              : "black",
-          backgroundSize: "100% 100%",
-          display: "flex",
-          flexDirection: "column",
-          flexWrap: "nowrap",
-          height: "100%",
-          justifyContent: "center",
-          textAlign: "center",
-          width: "100%",
-        }}
-      >
-        <img src={imgSrc} style={{ position: "absolute", width: 1600 }} />
-        <div
-          style={{
-            color: "white",
-            fontSize: 60,
-            fontStyle: "normal",
-            letterSpacing: "-0.025em",
-            lineHeight: 1.4,
-            marginTop: 30,
-            padding: "0 120px",
-            whiteSpace: "pre-wrap",
-            backgroundColor: "black",
-            textAlign: "center",
-          }}
-        >
-          {TITLE}
-        </div>
-        <br />
-        <div
-          style={{
-            color: "white",
-            fontSize: 30,
-            fontStyle: "normal",
-            letterSpacing: "-0.025em",
-            lineHeight: 1.4,
-            marginTop: 30,
-            padding: "0 120px",
-            whiteSpace: "pre-wrap",
-            backgroundColor: "black",
-            textAlign: "center",
-          }}
-        >
-          {description}
-        </div>
-      </div>
-    ),
-    intents: [<Button.Transaction target="/buy">Buy now</Button.Transaction>],
-  });
+//   return c.res({
+//     image: (
+//       <div
+//         style={{
+//           alignItems: "center",
+//           background:
+//             status === "response"
+//               ? "linear-gradient(to right, #432889, #17101F)"
+//               : "black",
+//           backgroundSize: "100% 100%",
+//           display: "flex",
+//           flexDirection: "column",
+//           flexWrap: "nowrap",
+//           height: "100%",
+//           justifyContent: "center",
+//           textAlign: "center",
+//           width: "100%",
+//         }}
+//       >
+//         <img src={imgSrc} style={{ position: "absolute", width: 1600 }} />
+//         <div
+//           style={{
+//             color: "white",
+//             fontSize: 60,
+//             fontStyle: "normal",
+//             letterSpacing: "-0.025em",
+//             lineHeight: 1.4,
+//             marginTop: 30,
+//             padding: "0 120px",
+//             whiteSpace: "pre-wrap",
+//             backgroundColor: "black",
+//             textAlign: "center",
+//           }}
+//         >
+//           {TITLE}
+//         </div>
+//         <br />
+//         <div
+//           style={{
+//             color: "white",
+//             fontSize: 30,
+//             fontStyle: "normal",
+//             letterSpacing: "-0.025em",
+//             lineHeight: 1.4,
+//             marginTop: 30,
+//             padding: "0 120px",
+//             whiteSpace: "pre-wrap",
+//             backgroundColor: "black",
+//             textAlign: "center",
+//           }}
+//         >
+//           {description}
+//         </div>
+//       </div>
+//     ),
+//     intents: [<Button.Transaction target="/buy">Buy now</Button.Transaction>],
+//   });
+// });
+
+const app = generateFloorStoreApp({
+  collectionName: "BasePaint",
+  slug: "basepaint",
+  description: ({ price, currency }) =>
+    `Purchase via Frame\nPrice: ${price} ${currency}`,
+  chainId: ChainId.BASE,
 });
 
 devtools(app, { serveStatic });
