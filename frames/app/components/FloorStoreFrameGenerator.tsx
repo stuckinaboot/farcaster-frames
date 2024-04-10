20da0c02afdb505597abd37aedf1e2f92bf57ec6
stuckinaboot <paluter@gmail.com>
2024-04-10 09:52:49 -0400

    update wip

---
+++ frames/app/components/FloorStoreFrameGenerator.tsx
@@ -0,0 +1,113 @@
+"use client";
+
+import { Button, Grid, TextField, Typography } from "@mui/material";
+import Link from "next/link";
+import { useState } from "react";
+import StyledLink from "./Link";
+
+export default function FloorStoreFrameGenerator() {
+  const [collectionUrl, setCollectionUrl] = useState("");
+
+  function slugFromCollectionUrl(url: string) {
+    const lastIdx = url.lastIndexOf("/");
+    return url.slice(lastIdx + 1);
+  }
+
+  const frameLink = `${
+    process.env.VERCEL_URL || "http://localhost:3000"
+  }/api/floor-store/${slugFromCollectionUrl(collectionUrl)}`;
+
+  return (
+    <Grid container justifyContent={"center"} spacing="8">
+      <Grid item xs={12} style={{ textAlign: "center" }}>
+        <Typography variant="h3">Generate Floor Store Frame</Typography>
+
+        <Typography variant="body1">
+          Create a farcaster frame that allows people to buy the floor NFT in
+          any collection with one tap. No code required.
+        </Typography>
+      </Grid>
+      <Grid
+        container
+        item
+        xs={12}
+        justifyContent={"center"}
+        style={{ textAlign: "center" }}
+        spacing={3}
+      >
+        <Grid container item xs={12} justifyContent={"center"}>
+          <Grid item xs={12}>
+            <Typography variant="h6">
+              Step 1: enter the OpenSea collection URL (ex.
+              https://opensea.io/collection/oviators)
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              variant="standard"
+              label="OpenSea Collection URL"
+              fullWidth
+              value={collectionUrl}
+              onChange={(e) => setCollectionUrl(e.target.value)}
+            />
+          </Grid>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="h6">
+            Step 2: Copy the link to your frame{" "}
+            <StyledLink url={frameLink}>{frameLink}</StyledLink>
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="h6">
+            Step 3: Create a farcaster cast, paste your frame link, and share!
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="h6">Running into issues?</Typography>
+          <Typography variant="body2">
+            1. No background image displays on the frame: Frame generation
+            relies on a PNG/JPEG collection image existing for the particular
+            NFT collection on OpenSea. If this does not exist on OpenSea, no
+            background image will display.
+            <br />
+            2. "No collection found" displays on the frame: This frame only
+            works for collections public exposed via the OpenSea API. If the
+            OpenSea collection URL does not point to a valid collection viewable
+            on OpenSea, the frame generation will not work properly and you will
+            see the no collection found error.
+          </Typography>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          container
+          justifyContent={"center"}
+          style={{ textAlign: "center" }}
+        >
+          <Grid item xs={12} sm={8}>
+            <Typography variant="caption">
+              This project was created and is provided open-source by Aspyn (
+              <StyledLink url={"https://warpcast.com/aspyn"}>
+                warpcast
+              </StyledLink>
+              ,{" "}
+              <StyledLink url={"https://twitter.com/AspynPalatnick"}>
+                twitter
+              </StyledLink>
+              . The source code for this project can be found on{" "}
+              <StyledLink
+                url={"https://github.com/stuckinaboot/farcaster-frames"}
+              >
+                GitHub
+              </StyledLink>
+              . Use this project at your own risk and discretion. While this
+              project is powered by the public OpenSea API, this is not an
+              official OpenSea tool.
+            </Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+}
--- frames/app/page.tsx
+++ frames/app/page.tsx
@@ -2,7 +2,7 @@ import { getFrameMetadata } from "frog/next";
 import type { Metadata } from "next";
 
 import styles from "./page.module.css";
-import { Button, Grid, TextField, Typography } from "@mui/material";
+import FloorStoreFrameGenerator from "./components/FloorStoreFrameGenerator";
 
 export async function generateMetadata(): Promise<Metadata> {
   const frameTags = await getFrameMetadata(
@@ -14,35 +14,9 @@ export async function generateMetadata(): Promise<Metadata> {
 }
 
 export default function Home() {
-  let val;
-  let frameLink;
   return (
     <main className={styles.main} style={{ width: "100%" }}>
-      <Grid container justifyContent={"center"} spacing="4">
-        <Grid item xs={12} style={{ textAlign: "center" }}>
-          <Typography variant="h4">Generate Floor Store Frame</Typography>
-        </Grid>
-        <Grid container item xs={12} justifyContent={"center"}>
-          <Grid item xs={10} sm={6}>
-            <TextField
-              variant="standard"
-              label="OpenSea Collection URL"
-              fullWidth
-            />
-          </Grid>
-        </Grid>
-        <Grid container item xs={12} justifyContent={"center"}>
-          <Grid item>
-            <Button variant="contained">Generate Floor Store frame</Button>
-          </Grid>
-        </Grid>
-        <Grid item xs={12} style={{ textAlign: "center" }}>
-          <Typography>Link to your frame:</Typography>
-          <Typography>
-            <a href={frameLink}>{frameLink}</a>
-          </Typography>
-        </Grid>
-      </Grid>
+      <FloorStoreFrameGenerator />
     </main>
   );
 }
