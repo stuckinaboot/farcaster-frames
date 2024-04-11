a6d844ddffe978eae9552493afa28dea3f0ed41a
stuckinaboot <paluter@gmail.com>
2024-04-10 21:55:57 -0400

    update domain

--- frames/app/api/logging.ts
+++ frames/app/api/logging.ts
@@ -4,9 +4,8 @@ export async function logEvent(event: any) {
   const payload = {
     api_key: "30ee0054-5f4a-4f57-b12f-68f3531aab65",
     collection: "farcaster-frame-floor",
-    json: JSON.stringify(event),
+    json: JSON.stringify({ ...event, url: process.env.NEXT_PUBLIC_VERCEL_URL }),
     timestamp: Math.floor(new Date().getTime() / 1000),
-    url: process.env.NEXT_PUBLIC_VERCEL_URL,
   };
 
   await fetch("https://api.graphjson.com/api/log", {
