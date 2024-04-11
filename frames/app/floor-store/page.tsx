811ccea69da94f9ccdca79c9277af8b51dda7a48
stuckinaboot <paluter@gmail.com>
2024-04-10 22:04:46 -0400

    update

---
+++ frames/app/floor-store/page.tsx
@@ -0,0 +1,22 @@
+import { getFrameMetadata } from "frog/next";
+import type { Metadata } from "next";
+
+import styles from "../page.module.css";
+import FloorStoreFrameGenerator from "../components/FloorStoreFrameGenerator";
+
+export async function generateMetadata(): Promise<Metadata> {
+  const frameTags = await getFrameMetadata(
+    `${process.env.VERCEL_URL || "http://localhost:3000"}/api`
+  );
+  return {
+    other: frameTags,
+  };
+}
+
+export default function Home() {
+  return (
+    <main className={styles.main} style={{ width: "100%" }}>
+      <FloorStoreFrameGenerator />
+    </main>
+  );
+}
